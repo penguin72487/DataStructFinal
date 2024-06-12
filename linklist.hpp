ad6c4e95d621aa35a08a572ba625a2f5b94b0781
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2024-06-12 18:09:25 +0800

    P1 Done

--- linklist.hpp
+++ linklist.hpp
@@ -132,9 +132,30 @@ class linklist{
         tail = nullptr;
         n = 0;
     }
+
+    void dailyPrint() {
+        int i = 0;
+        double previous_close = 0;
+        for (auto& it : *this) {
+            if (i != 0) {
+                cout << it.get_Date() << " " << (it.get_close() - previous_close) / previous_close * 100 << endl;
+            }
+            previous_close = it.get_close();
+            i++;
+        }
+    }
+    void intradailyPrint(){
+        int i=0;
+        for(auto& it:*this){
+            cout << it.get_Date() << " " << (it.get_close() - it.get_open()) / it.get_open() * 100 << endl;
+            i++;
+        }
+    }
     friend ostream& operator<<(ostream& os, const linklist& ll){
+        int i=0;
         for(auto& it:ll){
-            os<<it<<" ";
+            os<< it.get_Date() <<" "<< it.get_close() << endl;
+            i++;
         }
         return os;
     }

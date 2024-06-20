efdaa78777bc3d925f9e2ae599188540d99db9d9
SiroKu_ <56777898+SiroKu1006@users.noreply.github.com>
2024-06-20 22:50:45 +0800

    refactor: Update class name from k_Bar to k_Bar_Date in BtreapMain.cpp

--- BtreapMain.cpp
+++ BtreapMain.cpp
@@ -160,23 +160,61 @@ int main() {
     for (auto& it : k_line) {
         treapB.insert(it);
     }
-    Treap<k_Bar_Date> treap;
+    Treap<k_Bar_Close> treap_close;
+    Treap<k_Bar_Date> treap_Date;
     int dat = 0;
     linklist<k_Bar_Date> toListB = treapB.toList();
     for (auto& it : toListB) {
         if(dat%5==0){
-            treap.insert(k_Bar_Date(it.get_Date(), it.open, it.High, it.Low, it.close));
+            treap_close.insert(k_Bar_Close(it.get_Date(), it.open, it.High, it.Low, it.close));
+            treap_Date.insert(k_Bar_Date(it.get_Date(), it.open, it.High, it.Low, it.close));
         }
         dat++;
     }
-    cout <<"1: " << treap.size() << endl;
-    cout << "8: " << endl;
-    linklist<k_Bar_Date> toList = treap.toList();
-    float max5 =-1000000, min5 = 1000000;
+    linklist<k_Bar_Close> toList_Close = treap_close.toList();
+    cout <<"1: " << treap_Date.size() << endl;
+    //(2) 找到10個最小價格及其對應的日期。
+    cout << "2: Small 10" << endl;
+    Treap<k_Bar_Close> a, b;
+    treap_close.slip_By_Size(a, b, 10);
 
+    cout << a << endl;
+
+
+    //(3) 找到10個最大價格及其對應的日期。
+    cout << "3: Big 10" << endl;
+    Treap<k_Bar_Close> c, d;
+    treap_close.slip_By_Size(c, d, treap_close.size()-10);
+
+    cout << d << endl;
+
+
+    //(4) 找到中位價格及其發生日期。
+    cout << "4: Median" << endl;
+    if(treap_close.size()&1){
+        cout << treap_close[treap_close.size()/2].get_Date() << " " << treap_close[treap_close.size()/2].get_close() << endl;
+    }
+    else{
+        auto it = treap_close[treap_close.size()/2];
+        auto it2 = treap_close[treap_close.size()/2-1];
+        cout << it.get_Date() << " " << it.get_close() << endl;
+        cout << it2.get_Date() << " " << it2.get_close() << endl;
+        cout << "Average: " << (it.get_close()+it2.get_close())/2 << endl;
+    }
+
+    //(7) 繪製收盤價隨時間變化的圖表，其中x軸為日期索引，y軸為價格。
+    cout << "7: " << endl;
+    for (auto& it : toList_Date) {
+        cout << it.get_Date() << " " << it.get_close() << endl;
+    }
+
+    cout << "8: " << endl;
+    
+    float max5 =-1000000, min5 = 1000000;
+    linklist<k_Bar_Date> toList_Date = treap_Date.toList();
     int i = 0;
         double previous_close = 0;
-        for (auto& it : toList) {
+        for (auto& it : toListB) {
             if (i != 0) {
                 cout << it.get_Date() << " " << (it.get_close() - previous_close) / previous_close * 100 << endl;
                 if((it.get_close() - previous_close) / previous_close * 100 > max5){
@@ -193,6 +231,24 @@ int main() {
     cout << "5Max: " << max5 << endl;
     cout << "5Min: " << min5 << endl;
 
-
+    //(9) 繪製日內收益率隨時間變化的圖表，其中x軸為日期索引，y軸為日內收益率。
+    cout << "9: " << endl;
+    float max6 =-1000000, min6 = 1000000;
+    toList_Date = treap_Date.toList();
+    i = 0;
+        for (auto& it : toListB) {
+            if (i != 0) {
+                cout << it.get_Date() << " " << (it.get_close() - it.open) / it.open * 100 << endl;
+                if((it.get_close() - it.open) / it.open * 100 > max5){
+                    max6 = (it.get_close() - it.open) / it.open * 100;
+                }
+                if((it.get_close() - it.open) / it.open * 100 < min5){
+                    min6 = (it.get_close() - it.open) / it.open * 100;
+                }
+            }
+            i++;
+        }
+    cout << "6Max: " << max6 << endl;
+    cout << "6Min: " << min6 << endl;
     return 0;
 }
